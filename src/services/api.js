a98c91f661f8abab33c44035998aaf7f90335b75
babludangi6266 <babludangi2000@gmail.com>
2025-10-28 14:29:35 +0530

    base url changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+//const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://vendor-backend-0z0o.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
