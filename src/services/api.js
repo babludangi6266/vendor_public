581f1082a97467286a6eb5018debb346a8861581
babludangi6266 <babludangi2000@gmail.com>
2025-10-29 11:33:04 +0530

    .

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //const API_BASE_URL = 'http://localhost:5000/api';
-const API_BASE_URL = 'https://vendor-backend-up8d.onrender.com/api';
+const API_BASE_URL = 'https://vendor-backend-4v8l.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
